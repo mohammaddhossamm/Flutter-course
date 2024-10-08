d23bd0e053ef6b9fe1ddb016abe55ca6b42f1092
Mohamed Hossam <mohammaddhossamm@gmail.com>
2024-10-08 13:36:36 +0300

    login and custom TextFormField

---
+++ login/custom_tff.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+Widget CustomTextFormField({
+bool obscureText=false,
+String? Function(String?)? validator,
+required String labelText,
+Widget? prefixIcon,
+Widget? suffixIcon,
+}
+
+) {
+  return TextFormField(
+    obscureText: obscureText,
+    validator: validator,
+    decoration: InputDecoration(
+      labelText: labelText,
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(15),
+       borderSide: BorderSide(
+      color: Colors.blue,
+      width: 1,
+    ),
+      ),
+      prefixIcon: prefixIcon,
+      suffixIcon: suffixIcon,
+    ),
+  );
+}
